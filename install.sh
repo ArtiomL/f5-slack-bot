028999b99fcf278f413d796caa60b8a9d1596483
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-11-24 13:00:13 +0200

    Installation

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@
 # Artiom Lichtenstein
 # v1.0.1, 24/11/2017
 
+set -x
 tmsh create ilx workspace ilxws_SLACK_BOT from-uri https://raw.githubusercontent.com/ArtiomL/f5-slack-bot/master/bin/ilxws.tgz > /dev/null 2>&1
+set +x
 read -s -p "Enter Slack API Token: " str_AToken
+echo
 sed -i "s/|| ''/|| '$str_AToken'/" /var/ilx/workspaces/Common/ilxws_SLACK_BOT/extensions/ilxex_SLACK_BOT/index.js
+set -x
 nohup /usr/bin/nodejs /var/ilx/workspaces/Common/ilxws_SLACK_BOT/extensions/ilxex_SLACK_BOT/index.js > /var/log/f5-slack-bot.log &
