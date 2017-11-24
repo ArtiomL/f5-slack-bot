ac05439bcb77235a1ef769db075c62f7186eae28
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-11-24 11:06:31 +0200

    Installation

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# f5-slack-bot - install.sh
+# https://github.com/ArtiomL/f5-slack-bot
+# Artiom Lichtenstein
+# v1.0.0, 24/11/2017
 
+tmsh create ilx workspace ilxws_SLACK_BOT from-uri https://raw.githubusercontent.com/ArtiomL/f5-slack-bot/master/bin/ilxws.tgz > /dev/null 2>&1
